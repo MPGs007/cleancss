262d9aa9e2ca1b3088088c59fdd426f7758fb70f
noel krohn <noel@mpg-commercial.com>
2023-10-11 14:36:26 +0100

    First commit to git-base files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('back.layout.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Dashboard')
+@section('content')
+    This is the home content
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="flex flex-col mx-auto text-center text-lg text-blue-900">Here is the start</div>
+</body>
+</html>
